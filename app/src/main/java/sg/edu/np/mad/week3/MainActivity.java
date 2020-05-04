25e111e9a7647631a8f36b33d3dba58a84567590
Wen Cong Yeo <s10194723@connect.np.edu.sg>
2020-05-04 21:20:05 +0800

    Added xml and completed main activity xml. But couldn't find video/image

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,13 +2,19 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        video = findViewById(R.id.videoView);
+        video.setVideoURI(Uri.parse("android.resources://" + getPackageName() + "/" + R.raw.video));
+        video.start();
     }
 }
