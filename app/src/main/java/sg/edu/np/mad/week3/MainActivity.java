2fc010f66723a0dd165fff7f6b6047bd5d0d86f6
Wen Cong Yeo <s10194723@connect.np.edu.sg>
2020-05-05 18:51:00 +0800

    Added video, audio and xml and allow it to be played

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,19 +2,32 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
     VideoView video;
+    MediaPlayer audio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         video = findViewById(R.id.videoView);
-        video.setVideoURI(Uri.parse("android.resources://" + getPackageName() + "/" + R.raw.video));
+        video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
         video.start();
+
+        audio = MediaPlayer.create(this, R.raw.baby_laughing);
+        audio.start();
+        audio.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                audio.release();
+                audio = null;
+            }
+        });
+
     }
 }
